af56281f0df61ebd8db09570ac98bd8b67dcb4e5
gimganghwi <gimganghwi@gmail.com>
2021-01-19 16:39:19 +0900

    방명록 조회 기능 구현
    목록에서 방명록 고유 번호를 눌러서 방명록의 내용을 볼 수 있다.

--- guestbook/src/main/java/com/gimganghwi/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/gimganghwi/guestbook/controller/GuestbookController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -49,5 +50,15 @@ public class GuestbookController {
         redirectAttributes.addFlashAttribute("msg", gno);
 
         return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+
+        log.info("gno: " + gno);
+
+        GuestbookDTO dto = service.read(gno);
+
+        model.addAttribute("dto", dto);
     }
 }
--- guestbook/src/main/java/com/gimganghwi/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/com/gimganghwi/guestbook/service/GuestbookServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -34,6 +35,13 @@ public class GuestbookServiceImpl implements GuestbookService{
         repository.save(entity);
 
         return entity.getGno();
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.isPresent() ? entityToDTO(result.get()) : null;
     }
 
     @Override
