1bd1acf82f60c11b3e96c5d1be4ea29b97e82d53
gimganghwi <gimganghwi@gmail.com>
2021-01-19 16:17:27 +0900

    방명록 등록 기능 추가

--- guestbook/src/main/java/com/gimganghwi/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/gimganghwi/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.gimganghwi.guestbook.controller;
 
+import com.gimganghwi.guestbook.dto.GuestbookDTO;
 import com.gimganghwi.guestbook.dto.PageRequestDTO;
 import com.gimganghwi.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -30,5 +33,21 @@ public class GuestbookController {
         model.addAttribute("result", service.getList(pageRequestDTO));
 
         return "/guestbook/list";
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..." + dto);
+
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
